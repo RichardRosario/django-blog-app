e5392d41ffb8f0b014d49fdbce5a77e3d63cb3d5
Richard Rosario <tech2js@gmail.com>
2020-09-10 21:17:39 +0800

    created html templating for better blog display

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 1. setup python django environment
 2. publish first django web app
+3. created initial blog routes
+4. creating an html templating and displaying to browser
+5. Created dummy data and display on homepage
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Richard',
+        "title": 'Blog Post #1',
+        'content': 'first post contents',
+        'date_posted': 'Sept 10, 2020'
+    },
+    {
+        'author': 'Anna',
+        "title": 'Blog Post #2',
+        'content': 'Second post contents',
+        'date_posted': 'Sept 8, 2020'
+    },
+    {
+        'author': 'RichardR',
+        "title": 'Blog Post #3',
+        'content': 'third post contents',
+        'date_posted': 'Sept 2, 2020'
+    }
+]
 
 # function that handles the traffic to home page
 
 
 def home(request):
-    return HttpResponse('<h1>You are in the Blog Homepage</h1>')
+    # add a context
+    context = {
+        'posts': posts
+    }
+    # return HttpResponse('<doctype />...')
+    return render(request, 'blog/home.html', context)
 
 # function that handles the about page traffic
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
