acb95917625d8501d7e6b996ea45e823d36a06b7
Richard Rosario <tech2js@gmail.com>
2020-09-15 20:13:52 +0800

    created validtion messages and extends UserCreation class

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 3. created initial blog routes
 4. creating an html templating and displaying to browser
 5. Created dummy data and display on homepage
+6. created registration functionality with user validation
+7. created from validation messages
+8. applied bootstrap 4 styling
+9. created custom forms file to add more fields
--- users/views.py
+++ users/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+# register user and check if form is valid
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+         # save form data to database
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
