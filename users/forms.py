a016f147503b438356a858dabc6d931d31abe67e
Richard Rosario <tech2js@gmail.com>
2020-09-16 10:14:08 +0800

    added email duplicate validation on sign up

--- users/forms.py
+++ users/forms.py
@@ -6,6 +6,13 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'The Email is already in use.')
+        return email
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
