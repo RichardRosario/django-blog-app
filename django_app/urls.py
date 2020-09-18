14aa3b61cc1aa19f3af9997ec41c3b990562926b
Richard Rosario <tech2js@gmail.com>
2020-09-18 22:53:18 +0800

    created user profile and upload image functionality

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -16,3 +18,7 @@ urlpatterns = [
     path('', include('blog.urls'))
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
