3b829b60283d8650d6391623d3f159cd215192a0
Richard Rosario <tech2js@gmail.com>
2020-09-20 12:09:05 +0800

    added delete own post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import (PostListView, PostDetailView,
-                    PostCreateView, PostUpdateView)
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView)
 from . import views
 
 urlpatterns = [
@@ -8,6 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 
     path('about/', views.about, name='blog-about'),
