c377e25fec56f9aefe4b638a66ddd0ef19d5db00
Richard Rosario <tech2js@gmail.com>
2020-10-06 10:17:14 +0800

    added imagefield

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(
+        upload_to='media/', default='media/default.jpg')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
